cbf967d554d83f827d5637ea8ecf0f3527fda70d
evan-boissonnot <commercial@boissonnot.Fr>
2019-01-01 23:31:11 +0100

    discover arrow

---
+++ first-steps/with-aspnet-core/TypeScript.Functions/DiscoverArrowFunctions/wwwroot/TypeScript/example-arrow.ts
@@ -0,0 +1,11 @@
+﻿const sumNumbers = function (num1: number, num2: number) {
+    return num1 + num2;
+}
+
+const sumNumbers2 = (num1: number, num2: number): number => num1 + num2; 
+
+alert(sumNumbers(1, 2));
+
+alert(sumNumbers2(3, 4));
+
+console.log(sumNumbers(8, 1));
---
+++ first-steps/with-aspnet-core/TypeScript.Functions/DiscoverArrowFunctions/wwwroot/js/example-arrow.js
@@ -0,0 +1,8 @@
+const sumNumbers = function (num1, num2) {
+    return num1 + num2;
+};
+const sumNumbers2 = (num1, num2) => num1 + num2;
+alert(sumNumbers(1, 2));
+alert(sumNumbers2(3, 4));
+console.log(sumNumbers(8, 1));
+//# sourceMappingURL=example-arrow.js.map
